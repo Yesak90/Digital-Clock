9a3eb0d5440de072a0bab6a19cb2a44f268a8d13
Isaac Tiku <isaac.tiku1990@gmail.com>
2023-09-27 08:28:44 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+function updateTime() {
+  var now = new Date();
+
+  document.getElementById("time").innerHTML =
+    zeroPadding(now.getHours(), 2) +
+    ":" +
+    zeroPadding(now.getMinutes(), 2) +
+    ":" +
+    zeroPadding(now.getSeconds(), 2);
+
+  document.getElementById("date").innerHTML =
+    now.getFullYear() +
+    "-" +
+    zeroPadding(now.getMonth() + 1, 2) +
+    "-" +
+    zeroPadding(now.getDate(), 2) +
+    " " +
+    WEEK[now.getDay()];
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+function zeroPadding(num, digit) {
+  var zero = "";
+  for (var i = 0; i < digit; i++) {
+    zero += "0";
+  }
+  return (zero + num).slice(-digit);
+}
